f05e93569dcdc7dff749b353aa5eb9554b1af156
Amalie Smidth <amaliesmidth@gmail.com>
2018-10-02 18:31:05 +0100

    updated cli and run files

--- bin/run.rb
+++ bin/run.rb
@@ -2,7 +2,9 @@ require_relative '../config/environment'
 
 #harry_potter_api = RestClient.get("https://www.potterapi.com/v1/characters?key=$2a$10$GyAEHpxPsTfoiKHANOUWiOviE8TjqBoYk99ZGZ4dAXwu65dMGerIi")
 
-welcome
-get_user_name_and_create
-get_spellbook_name
-spellbook_instructions
+cli = CLI.new
+
+cli.welcome
+cli.get_user_name_and_create
+cli.get_spellbook_name
+cli.spellbook_instructions_and_choice
--- lib/cli.rb
+++ lib/cli.rb
@@ -1,3 +1,5 @@
+class CLI
+
 def welcome
   puts "Good day! My name is Professor Flitwick and will be helping you to create your very first Spell Book. What is your Wizard name?"
 end
@@ -14,13 +16,48 @@ def get_spellbook_name
   Spellbook.create(name: sb_name, user_id: User.last.id)
 end
 
-def spellbook_instructions
+def spellbook_instructions_and_choice
   spell_types = ["curses", "cexes", "charms", "enchantments", "spells"]
+  first_choice = ""
   puts "There are five kinds of spells: curses, hexes, charms, enchantments and regular spells. Which would you like to explore first?"
-  first_choice = gets.chomp
-  if spell_types.include?(first_choice.downcase)
-    puts "Great! Let's explore #{first_choice}!"
-  else
-    puts "You sound like a Muggle...that is not a type of spell, fool!"
+    loop do
+    first_choice = gets.chomp
+    if first_choice.downcase == "curses"
+      show_random_curse
+      break
+    elsif first_choice.downcase == "hexes"
+      show_random_hex
+      break
+    elsif first_choice.downcase == "charms"
+      show_random_charm
+      break
+    elsif first_choice.downcase == "enchantments"
+      show_random_enchantment
+      break
+    elsif first_choice.downcase == "spells"
+      show_random_spell
+      break
+    else
+      puts " "
+      puts "You spell like a Muggle...that is not magic, please pick again..."
+      puts " "
+      puts "CURSES"
+      puts "HEXES"
+      puts "CHARMS"
+      puts "ENCHANTMENTS"
+      puts "SPELLS"
+    end
   end
 end
+
+
+
+end
+
+  # puts "There are five kinds of spells: curses, hexes, charms, enchantments and regular spells. Which would you like to explore first?"
+  # first_choice = gets.chomp
+  # if spell_types.include?(first_choice.downcase)
+  #   puts "Great! Let's explore #{first_choice}!"
+  # else
+  #   puts "You sound like a Muggle...that is not a type of spell, fool! Please choose again."
+  # end
