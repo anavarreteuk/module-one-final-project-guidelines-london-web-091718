08e0b787979162c107fae235ad71c47db5886ba8
Amalie Smidth <amaliesmidth@gmail.com>
2018-10-02 13:05:18 +0100

    added gems

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,6 @@
 require_relative '../config/environment'
 
+#harry_potter_api = RestClient.get("https://www.potterapi.com/v1/characters?key=$2a$10$GyAEHpxPsTfoiKHANOUWiOviE8TjqBoYk99ZGZ4dAXwu65dMGerIi")
 
 
 puts "HELLO WORLD"
